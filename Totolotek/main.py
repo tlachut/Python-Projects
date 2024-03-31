f577a6e51ce08854440627e55e9a220736240a92
Tomasz Łachut <tomus.lachut@gmail.com>
2024-03-31 23:52:19 +0200

    Dodano wprowadzanie 6 liczb w konsoli przez użytkownika

--- Totolotek/main.py
+++ Totolotek/main.py
@@ -1,16 +1,32 @@
 # Symulacja losowania totolotka
 
 import random
+import math
 
 # Losowanie liczb komputera
 
 wylosowaneLiczby = []
 
-def sprawdzCzyWystepujePowtorkaKomputer():
+def sprawdzPoprawnoscKomputer():
     wylosowanaLiczba = random.randint(1, 49)
     if wylosowanaLiczba not in wylosowaneLiczby:
         wylosowaneLiczby.append(wylosowanaLiczba)
-    else: sprawdzCzyWystepujePowtorkaKomputer()
+    else: sprawdzPoprawnoscKomputer()
 
 for i in range(6):
-    sprawdzCzyWystepujePowtorkaKomputer()
+    sprawdzPoprawnoscKomputer()
+
+# Wprowadzanie liczb przez użytkownika
+
+liczbyUzytkownika = []
+
+def sprawdzPoprawnoscUzytkownik():
+    wprowadzonaLiczba = int(input(f"Wprowadź {i+1}. liczbę: "))
+    if wprowadzonaLiczba not in liczbyUzytkownika and wprowadzonaLiczba <= 49 and wprowadzonaLiczba >= 1:
+        liczbyUzytkownika.append(wprowadzonaLiczba)
+    else:
+        print("Błąd!")
+        sprawdzPoprawnoscUzytkownik()
+
+for i in range(6):
+    sprawdzPoprawnoscUzytkownik()
